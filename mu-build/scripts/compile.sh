8bf6f3ab50917b410d9aad5f9b3f03714621735b
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-05-10 09:36:49 +0200

    Add instruction on how to build Mu

--- mu-build/scripts/compile.sh
+++ mu-build/scripts/compile.sh
@@ -15,6 +15,16 @@ export PATH="$(pwd)${PATH:+:$PATH}"
 
 cd ..
 ./build.py distclean
+
+while true; do
+	read -p "Did you appropriately configure the core pinning in $(realpath crash-consensus/src/config.hpp)?[y/n] " yn
+    case $yn in
+        [Yy]* ) echo "Great! Continuing with building"; break;;
+        [Nn]* ) echo "Please, configure the core-pinning before continuing"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 ./build.py -c gcc crash-consensus
 
 crash-consensus/demo/using_conan_fully/build.sh gcc-release
@@ -25,3 +35,6 @@ mkdir -p prebuilt-lib/lib
 ./export.sh gcc-minsizerel && mkdir prebuilt-lib/lib/minsizerel && cp exported/libcrashconsensus.so prebuilt-lib/lib/minsizerel
 ./export.sh gcc-release && mkdir prebuilt-lib/lib/release && cp exported/libcrashconsensus.so prebuilt-lib/lib/release
 ./export.sh gcc-relwithdebinfo && mkdir prebuilt-lib/lib/relwithdebinfo && cp exported/libcrashconsensus.so prebuilt-lib/lib/relwithdebinfo
+
+cd ../..
+./build.py distclean
