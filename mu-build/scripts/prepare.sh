1287bb3db937b419bc88f3a7e5eb1795c5ab5226
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-05-12 21:15:49 +0200

    Add patch to set the cores in mu

--- mu-build/scripts/prepare.sh
+++ mu-build/scripts/prepare.sh
@@ -10,4 +10,5 @@ cd mu
 git revert -n 9b480342a28b2600f9d2ebec5e1cbb4b76148a00 &>/dev/null
 git revert -n 1c846cd76b4d535e646bf581724d894d54b1591d &>/dev/null
 git restore --staged crash-consensus/experiments/liquibook/liquibook_additions.tar.gz &>/dev/null
-patch -p 1 < ../thread_sleep.patch
+patch -p 1 < ../patches/thread_sleep.patch
+patch -p 1 < ../patches/default_cores.patch
