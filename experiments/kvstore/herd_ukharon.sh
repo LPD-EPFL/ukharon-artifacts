186929730b89ac10e31f6f707b823d90c7bc942e
Antoine Murat <antoine.murat@protonmail.com>
2022-05-12 21:56:45 +0200

    Update herd_ukharon.sh
    Reduced the explored parameter space

--- experiments/kvstore/herd_ukharon.sh
+++ experiments/kvstore/herd_ukharon.sh
@@ -20,7 +20,7 @@ reset_processes
 # uKharon-KV failover without cache
 # The number of iterations is just a demo.
 # Run 1000 iterations for robust results
-for i in {1..5}; do
+for i in {1..1}; do
   ./herd_ukharon_helper.sh put majority --failover
   reset_processes
   grep "Time to switch (us)" logs/herd_ukharon/latency_put_majority_failover/m4/logs/herd-client.txt >> logs/herd_ukharon/ukharonkv_failover_without_cache.txt
@@ -29,7 +29,7 @@ done
 # uKharon-KV failover with cache
 # The number of iterations is just a demo.
 # Run 1000 iterations for robust results
-for i in {1..5}; do
+for i in {1..1}; do
   ./herd_ukharon_helper.sh put cache --failover
   reset_processes
   grep "Time to switch (us)" logs/herd_ukharon/latency_put_cache_failover/m4/logs/herd-client.txt >> logs/herd_ukharon/ukharonkv_failover_with_cache.txt
