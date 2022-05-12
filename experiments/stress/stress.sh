19ecacfe326187e4246c28469e9b69f2a2ada470
Antoine Murat <antoine.murat@protonmail.com>
2022-05-12 16:03:46 +0200

    Update stress.sh

--- experiments/stress/stress.sh
+++ experiments/stress/stress.sh
@@ -14,7 +14,7 @@ send_payload "$SCRIPT_DIR"/payload.zip
 # for memload in $(seq 0 $(./mem_computation.py -c $UKHARON_MEMSTRESS_CORES --count)); do
 #     for netload in 0 30 60 80 100; do
 #        for delta in {15..30}; do
-for memload in $(./mem_computation.py -c $UKHARON_MEMSTRESS_CORES -n 3 --count || echo 0); do
+for memload in $(./mem_computation.py -c $UKHARON_MEMSTRESS_CORES -n 3 --count 2>/dev/null || echo 0); do
     for netload in 80; do
         for delta in 25; do
             ./stress_helper.sh $delta $netload $memload
